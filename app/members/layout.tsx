6307b458f75f1301387eef112732e9edd64063fb
Riot Bryant <258329960+RiotBryant@users.noreply.github.com>
2026-02-17 11:04:15 -0500

    Create layout.tsx

---
+++ app/members/layout.tsx
@@ -0,0 +1,5 @@
+import PortalShell from "@/components/PortalShell";
+
+export default function MembersLayout({ children }: { children: React.ReactNode }) {
+  return <PortalShell>{children}</PortalShell>;
+}
