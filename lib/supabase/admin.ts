ca7dc769de592ffa443d426ddcc35813eeb5b816
Riot Bryant <258329960+RiotBryant@users.noreply.github.com>
2026-02-17 13:00:03 -0500

    Update admin.ts

--- lib/supabase/admin.ts
+++ lib/supabase/admin.ts
@@ -3,6 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 export function createAdminClient() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4dGpoa3Vta2dlbGd2bGl1ZnVsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDQxNjE1MCwiZXhwIjoyMDg1OTkyMTUwfQ.TCsCVaoas25QYYLt5mwZZwhkXbmfwemaU9i1cxp8PqE!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 }
